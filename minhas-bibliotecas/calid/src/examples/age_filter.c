7feca9819e0a78d1bdc4161889d5690f5e502b9f
miguel-webplace <minuz.code@gmail.com>
2026-09-22 19:31:50 -0300

    - Add debug directive - Remodel method to create checklists - Remodel lib interface in 'age_filter' - Encapsulate checklist struct to contain the size - Develop filter objects struture and usage - Create new sandbox to play with void pointers
    [ See 'DIARY.md' to analyse the changes with time ]

--- minhas-bibliotecas/calid/src/examples/age_filter.c
+++ minhas-bibliotecas/calid/src/examples/age_filter.c
@@ -4,20 +4,19 @@
 // Example of filling a age field
 
 int main() {
-
-
+    
+    
     // Variable and pointer to hold the data
-    int user_age, *ptr_user_age;
-
+    int user_age, *ptr_user_age = &user_age;
+    
     Form *form = create_form();
-
-    // Create a stack in dynamic memory for all the validations
-    Checklist *age_filters = create_checklist();
-
-    // Allocating (or reallocating) memory for the filters
-    add_check(&age_filters, greater_than(18));
-    add_check(&age_filters, less_than(70));
-
+    
+    // create checklist passing the number of items
+    CheckList age_filters = {.size = 2};
+    
+    // add the filters to 'checklist'
+    create_checklist(&age_filters, greater_than(18), less_than(70));
+    
     // include fields on the same pointer, as a form
     add_field(
         &form,
