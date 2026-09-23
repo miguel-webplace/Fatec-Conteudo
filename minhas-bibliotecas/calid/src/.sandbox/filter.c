3050cda4784ac90bbdabd1a45ced206b94757cd5
miguel-webplace <minuz.code@gmail.com>
2026-09-23 19:11:34 -0300

    - resolve 'va_arg' issue - remove undetected user-after-free bug
    See 'DIARY.md' for more details

--- minhas-bibliotecas/calid/src/.sandbox/filter.c
+++ minhas-bibliotecas/calid/src/.sandbox/filter.c
@@ -1,53 +1,61 @@
 #include "check_alloc_pointer.c"
 
 #define DEBUG
-
-int _eq(void* value, void* ref) {
-    return *(int*)value == *(int*)ref;
-}
-
-CheckItem equal(int ref) {
-
-    CheckItem eq = {
-        .verify = &_eq,
-        .ref = &ref
-    };
-    return eq;
-}
-
-int _gt(void* value, void* ref) {
-    return *(int*)value >= *(int*)ref;
-}
-
-CheckItem greater_than(int ref) {
-
-    CheckItem gt = {
-        .verify = &_gt,
-        .ref = &ref
-    };
-    return gt;
-}
-
+#define DEBUG_MAIN
 
 int _lt(void* value, void* ref) {
     return *(int*)value <= *(int*)ref;
 }
 
-CheckItem less_than(int ref) {
+CheckItem less_than(void* ref) {
     CheckItem ls = {
         .verify = &_lt,
-        .ref = &ref
+        .ref = ref
     };
     return ls;
 }
 
+// --------------------------------------------------
 
+int _eq(void* value, void* ref) {
+    return *(int*)value == *(int*)ref;
+}
 
-#ifdef DEBUG
-int main() {
-    int ipt_age_1 = 10, ipt_age_2 = 18, ipt_age_3 = 80;
+CheckItem equal(void* ref) {
     
-    CheckList age_filter = create_checklist(3, greater_than(18), less_than(100), equal(68));
+    CheckItem eq = {
+        .verify = &_eq,
+        .ref = ref
+    };
+    return eq;
+}
+
+// --------------------------------------------------
+
+int _gt(void* value, void* ref) {
+    return *(int*)value >= *(int*)ref;
+}
+
+CheckItem greater_than(void *ref) {
+        
+    CheckItem gt = {
+        .verify = &_gt,
+        .ref = ref
+    };
+    return gt;
+}
+
+// --------------------------------------------------
+
+#ifdef DEBUG_MAIN
+int main() {
+    int ipt_age_1 = 10, ipt_age_2 = 18, ipt_age_3 = 90;
+
+    int AGE_18 = 18, AGE_90 = 80;
+    
+    // CheckList age_filter = create_checklist(2, greater_than(&AGE_18), less_than(&AGE_90));
+    CheckList age_filter = {.size = 2};
+    create_checklist(&age_filter, greater_than(&AGE_18), less_than(&AGE_90));
     
     printf(" --- DEBUG --- : 'greater_than' ref value: %d\n", *(int*) age_filter.list[0].ref);
     printf(" --- DEBUG --- : 'less_than' ref value: %d\n\n", *(int*) age_filter.list[1].ref);
